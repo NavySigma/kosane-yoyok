6ef1514d76ba79ba3202f0098e18908f37f367d2
Disturw <kaku87237@gmail.com>
2026-01-21 18:45:29 +0700

    Mengosongkan semua isi branch

--- src/components/StatCard.jsx
+++
@@ -1,8 +0,0 @@
-export default function StatCard({ title, value }) {
-  return (
-    <div className="bg-[#F7F5EC] rounded-2xl p-5 shadow">
-      <p className="text-sm text-[#1E1B6D] mb-2">{title}</p>
-      <h3 className="text-xl font-bold text-[#1E1B6D]">{value}</h3>
-    </div>
-  );
-}
